0ccd663029b7e01aeeace20cd1dae8860a589fb2
Charlie Jackson <contact@charliejackson.com>
2017-07-06 17:57:52 +0100

    Removed redundant sync button

--- src/components/LoopView/LoopView.js
+++ src/components/LoopView/LoopView.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import MantraLoop from 'containers/MantraLoop/MantraLoop';
 import AddButton from 'containers/AddButton/AddButton';
-import Network from 'containers/Network/Network';
 import style from 'components/LoopView/LoopView.style';
 
 const LoopView = () => (
   <View style={style.container}>
     <StatusBar barStyle="dark-content" />
     <MantraLoop />
-    <Network />
     <AddButton />
   </View>
 );
