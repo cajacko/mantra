6570357354d1499ca167302e124986e3f60a70ae
Charlie Jackson <contact@charliejackson.com>
2017-08-16 16:46:01 +0100

    Reset upcoming notifications if notification settings change

--- src/reducers/lastSetNotifications.js
+++ src/reducers/lastSetNotifications.js
@@ -1,6 +1,8 @@
 export default (state = null, { type, payload }) => {
   switch (type) {
     case 'LOGOUT':
+    case 'SET_NOTIFICATIONS':
+    case 'SET_PERMISSIONS':
       return null;
 
     case 'LAST_SET_NOTIFICATIONS':
