837e6d149de540a3c0b49d7e79772c6f2e83d028
Charlie Jackson <charlie@charliejackson.com>
2017-10-01 09:43:26 +0100

    Implemented next button logic

--- src/components/WelcomeView/WelcomeView.component.js
+++ src/components/WelcomeView/WelcomeView.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StatusBar, Text, Image } from 'react-native';
 import Swiper from 'react-native-swiper';
 import displayNavStyle from 'components/DisplayNav/DisplayNav.style';
@@ -17,33 +17,66 @@ const welcomeScreens = [
 
 // Cannot seem to have a custom Component as a child of react-native-swiper.
 // Kept on breaking. So have put the actual markup in this file
-const WelcomeView = () => (
-  <View style={style.container}>
-    <StatusBar barStyle="dark-content" />
-    <Swiper
-      showsButtons={false}
-      buttonWrapperStyle={displayNavStyle.wrapper}
-      showsPagination
-      loop={false}
-    >
-      {welcomeScreens &&
-        welcomeScreens.map(({ title, description, image }, index) => (
-          <View key={title} style={style.screen}>
-            <Image resizeMode="contain" source={image} style={style.image} />
-            <View style={style.textContent}>
-              <Text style={style.title}>{title}</Text>
-              <View style={style.button}>
-                {index === welcomeScreens.length - 1 ? (
-                  <Button text="Done" size="small" />
-                ) : (
-                  <Button text="Next" theme="dull" size="small" />
-                )}
+class WelcomeView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.next = this.next.bind(this);
+    this.finish = this.finish.bind(this);
+  }
+
+  next() {
+    if (this.swiper && typeof this.swiper.scrollBy === 'function') {
+      this.swiper.scrollBy(1, true);
+    }
+  }
+
+  finish() {
+    console.log('FINISH');
+  }
+
+  render() {
+    return (
+      <View style={style.container}>
+        <StatusBar barStyle="dark-content" />
+        <Swiper
+          showsButtons={false}
+          buttonWrapperStyle={displayNavStyle.wrapper}
+          showsPagination
+          loop={false}
+          ref={(swiper) => {
+            this.swiper = swiper;
+          }}
+        >
+          {welcomeScreens &&
+            welcomeScreens.map(({ title, description, image }, index) => (
+              <View key={title} style={style.screen}>
+                <Image
+                  resizeMode="contain"
+                  source={image}
+                  style={style.image}
+                />
+                <View style={style.textContent}>
+                  <Text style={style.title}>{title}</Text>
+                  <View style={style.button}>
+                    {index === welcomeScreens.length - 1 ? (
+                      <Button text="Done" size="small" onPress={this.finish} />
+                    ) : (
+                      <Button
+                        text="Next"
+                        theme="dull"
+                        size="small"
+                        onPress={this.next}
+                      />
+                    )}
+                  </View>
+                </View>
               </View>
-            </View>
-          </View>
-        ))}
-    </Swiper>
-  </View>
-);
+            ))}
+        </Swiper>
+      </View>
+    );
+  }
+}
 
 export default WelcomeView;
