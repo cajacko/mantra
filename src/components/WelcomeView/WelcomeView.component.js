1ddbff887d590c83172f897da8a0cdf4ee111e07
Charlie Jackson <charlie@charliejackson.com>
2017-10-01 09:16:10 +0100

    Added slider with images

--- src/components/WelcomeView/WelcomeView.component.js
+++ src/components/WelcomeView/WelcomeView.component.js
@@ -1,36 +1,47 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, Text, Image } from 'react-native';
 import Swiper from 'react-native-swiper';
 import displayNavStyle from 'components/DisplayNav/DisplayNav.style';
 import style from 'components/WelcomeView/WelcomeView.style';
-import WelcomePanel from 'components/WelcomePanel/WelcomePanel.component';
+import Button from 'components/Button/Button';
 
-const dummyImage = require('components/WelcomeView/app.png');
+const home = require('components/WelcomeView/home.jpg');
+const list = require('components/WelcomeView/list.jpg');
+const add = require('components/WelcomeView/add.jpg');
 
 const welcomeScreens = [
-  { title: 'Welcome to mantra', description: 'Here we are', image: dummyImage },
-  { title: 'Page 2', description: 'Yeah boi', image: dummyImage },
-  { title: 'Page 3', description: 'Woo woo', image: dummyImage },
+  { title: 'Welcome to mantra', image: home },
+  { title: 'Page 2', image: list },
+  { title: 'Page 3', image: add },
 ];
 
+// Cannot seem to have a custom Component as a child of react-native-swiper.
+// Kept on breaking. So have put the actual markup in this file
 const WelcomeView = () => (
   <View style={style.container}>
     <StatusBar barStyle="dark-content" />
     <Swiper
       showsButtons={false}
-      loadMinimal
       buttonWrapperStyle={displayNavStyle.wrapper}
       showsPagination
       loop={false}
     >
-      {welcomeScreens.map(({ title, description, image }) => (
-        <WelcomePanel
-          key={title}
-          title={title}
-          description={description}
-          image={image}
-        />
-      ))}
+      {welcomeScreens &&
+        welcomeScreens.map(({ title, description, image }, index) => (
+          <View key={title} style={style.screen}>
+            <Image resizeMode="contain" source={image} style={style.image} />
+            <View style={style.textContent}>
+              <Text style={style.title}>{title}</Text>
+              <View style={style.button}>
+                {index === welcomeScreens.length ? (
+                  <Button text="Done" size="small" />
+                ) : (
+                  <Button text="Next" theme="dull" size="small" />
+                )}
+              </View>
+            </View>
+          </View>
+        ))}
     </Swiper>
   </View>
 );
--- src/components/WelcomeView/WelcomeView.style.js
+++ src/components/WelcomeView/WelcomeView.style.js
@@ -1,10 +1,43 @@
 import statusBarHeight from 'helpers/statusBarHeight';
-import { WHITE } from 'constants/colours';
+import { WHITE, BLACK } from 'constants/colours';
+import { TEXT_SIZES } from 'constants/text';
 
 export default {
   container: {
     flex: 1,
     paddingTop: statusBarHeight(),
     backgroundColor: WHITE,
+  },
+
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 30,
+  },
+
+  image: {
+    flex: 2,
+    shadowColor: BLACK,
+    shadowOpacity: 0.3,
+    shadowOffset: { width: -2, height: 2 },
+    shadowRadius: 5,
+  },
+
+  title: {
+    fontSize: TEXT_SIZES.MEDIUM,
+  },
+
+  textContent: {
+    flex: 1,
+    paddingTop: 40,
+    alignItems: 'center',
+  },
+
+  button: {
+    marginTop: 20,
+    width: 100,
   },
 };
