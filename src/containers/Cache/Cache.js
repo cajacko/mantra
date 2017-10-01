5cc9ddeff978842a9460aac910e3f20cffd945c0
Charlie Jackson <charlie@charliejackson.com>
2017-10-01 15:56:19 +0100

    Preload welcome view images

--- src/containers/Cache/Cache.js
+++ src/containers/Cache/Cache.js
@@ -1,7 +1,21 @@
+/* eslint global-require: 0 */
 import React, { Component } from 'react';
-import { AppLoading, Font } from 'expo';
+import { Image } from 'react-native';
+import { AppLoading, Font, Asset } from 'expo';
 import PropTypes from 'prop-types';
+// eslint-disable-next-line
 import { Ionicons } from '@expo/vector-icons';
+// import imageMap from 'constants/imageMap';
+
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    }
+
+    return Asset.fromModule(image).downloadAsync();
+  });
+}
 
 function cacheFonts(fonts) {
   return fonts.map(font => Font.loadAsync(font));
@@ -18,8 +32,15 @@ class Cache extends Component {
   }
 
   async loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('components/WelcomeView/home.jpg'),
+      require('components/WelcomeView/list.jpg'),
+      require('components/WelcomeView/add.jpg'),
+    ]);
+
     const fontAssets = cacheFonts([Ionicons.font]);
-    await Promise.all([...fontAssets]);
+
+    await Promise.all([...fontAssets, ...imageAssets]);
 
     this.setState({ appIsReady: true });
   }
