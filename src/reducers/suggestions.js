dc629b9013c11336eea15c50a516a48e0db07c43
Charlie Jackson <charlie@charliejackson.com>
2017-10-02 22:33:54 +0100

    Hardcoded suggestion mantra

--- src/reducers/suggestions.js
+++ src/reducers/suggestions.js
@@ -1,3 +1,11 @@
 const defaultState = {};
 
-export default (state = defaultState) => state;
+for (let i = 0; i < 20; i += 1) {
+  defaultState[i] = {
+    title: `Suggestion ${i}`,
+    id: i,
+    order: 1,
+  };
+}
+
+export default () => defaultState;
