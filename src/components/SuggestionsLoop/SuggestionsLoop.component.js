4febf0c1c350abf05ec9ea9c21ff56e28e61ce5a
Charlie Jackson <charlie@charliejackson.com>
2017-10-03 08:40:15 +0100

    Added documentation for suggestions loop

--- src/components/SuggestionsLoop/SuggestionsLoop.component.js
+++ src/components/SuggestionsLoop/SuggestionsLoop.component.js
@@ -3,6 +3,12 @@ import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 import Mantra from 'components/Mantra/Mantra.render';
 
+/**
+ * Render a list of suggested Mantra
+ *
+ * @param {array} suggestions Array of suggested Mantra
+ * @return {Component} JSX component to render
+ */
 const SuggestionsLoop = ({ suggestions }) => (
   <FlatList
     data={suggestions}
