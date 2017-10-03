947d007c2a7d4ea369cd5c30d4dcc4dd8331d615
Charlie Jackson <charlie@charliejackson.com>
2017-10-03 11:06:12 +0100

    Filter suggestions by those added

--- src/components/SuggestionsView/SuggestionsView.container.js
+++ src/components/SuggestionsView/SuggestionsView.container.js
@@ -4,12 +4,32 @@ import { connect } from 'react-redux';
 import SuggestionsView from 'components/SuggestionsView/SuggestionsView.component';
 
 /**
- * Map store state to component props
+ * Map store state to component props. Filter the suggestions by which ones are
+ * in added suggestions and discard suggestions
+ *
  * @param  {object} suggestions Keyed mantra suggestions
  * @return {object}             Props to pass to component
  */
-const mapStateToProps = ({ suggestions }) => ({
+const mapStateToProps = ({
   suggestions,
-});
+  addedSuggestions,
+  discardedSuggestions,
+}) => {
+  const items = suggestions.filter(({ id }) => {
+    if (addedSuggestions.includes(id)) {
+      return false;
+    }
+
+    if (discardedSuggestions.includes(id)) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return {
+    suggestions: items,
+  };
+};
 
 export default connect(mapStateToProps, undefined)(SuggestionsView);
