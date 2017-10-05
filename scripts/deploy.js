be711393aa8714007d4fb774e6e39197f8f99597
Charlie Jackson <charlie@charliejackson.com>
2017-10-05 20:22:07 +0100

    Updated publish scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ import { join } from 'path';
 copySync(
   join(
     __dirname,
-    process.argv[2].includes('live') ? '../app.live.json' : '../app.test.json',
+    process.argv[2].includes('live') ? '../app.live.json' : '../app.dev.json',
   ),
   join(__dirname, '../app.json'),
 );
