55a9e80b51df81dfcb5f91fba4f82183b885527e
Charlie Jackson <charlie@charliejackson.com>
2017-10-06 08:07:32 +0100

    Added download build function

--- scripts/expoBuild.js
+++ scripts/expoBuild.js
@@ -1,27 +1,36 @@
+/* eslint no-console: 0 */
 import { execSync } from 'child_process';
+import download from 'download-file';
 
 function build() {
-  const output = execSync('npm run build:ios', { encoding: 'utf8' });
+  return new Promise((resolve, reject) => {
+    resolve();
 
-  if (output.includes("What's your Apple ID")) {
-    throw new Error(
-      'No apple credentials, run "npm run build:ios" to enter credentials',
-    );
-  }
+    // const output = execSync('npm run build:ios', { encoding: 'utf8' });
+    //
+    // if (output.includes("What's your Apple ID")) {
+    //   reject(
+    //     'No apple credentials, run "npm run build:ios" to enter credentials',
+    //   );
+    //   return;
+    // }
+    //
+    // resolve();
+  });
 }
 
 function checkStatus() {
   return new Promise((resolve, reject) => {
     // stream so is nicer output
     const output = execSync('npm run build:status', { encoding: 'utf8' });
-    // console.log(output);
+    console.log(output);
 
     const matches = output.match(/\bhttps?:\/\/\S+/gi);
     const buildUrl = matches && matches[0] ? matches[0] : null;
 
     if (buildUrl) {
       resolve(buildUrl);
-    } else if (output.includes('in progess?!?!?!?')) {
+    } else if (output.includes('Build in progress')) {
       resolve();
     } else {
       reject();
@@ -29,7 +38,9 @@ function checkStatus() {
   });
 }
 
-function waitForStatus(i) {
+function waitForStatus(index) {
+  const i = index || 0;
+
   return new Promise((resolve, reject) => {
     if (i > 100) {
       reject();
@@ -54,7 +65,28 @@ function waitForStatus(i) {
   });
 }
 
-// build();
-waitForStatus(0)
+function downloadBuild(url) {
+  return new Promise((resolve, reject) => {
+    const directory = './tmp/';
+    const filename = 'build.ipa';
+
+    console.log(`Downloading build: ${url}`);
+
+    download(url, { directory, filename }, (err) => {
+      if (err) reject(err);
+      const filepath = `${directory}${filename}`;
+      console.log(`Build downloaded at: ${filepath}`);
+      resolve(filepath);
+    });
+
+    resolve();
+  });
+}
+
+build()
+  .then(waitForStatus)
+  .then(downloadBuild)
   .then(console.log)
-  .catch(() => console.log('ERROR oh no'));
+  .catch((err) => {
+    throw new Error(err || 'Undefined application error');
+  });
