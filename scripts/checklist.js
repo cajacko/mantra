662ad813a1903f2c1a6d109c6661f102fa87c6f7
Charlie Jackson <charlie@charliejackson.com>
2017-10-07 14:51:29 +0100

    Starting add task
    Trello card: https://trello.com/c/LZPKBkZ4

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -1,19 +1,59 @@
 import inquirer from 'inquirer';
 
+function reorder(callback) {
+  return inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'reorder',
+        message: 'Reordering?',
+      },
+    ])
+    .then(() => callback());
+}
+
+function add(callback) {
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Add a title for the question?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Add additional information',
+      },
+      {
+        type: 'confirm',
+        name: 'reorder',
+        message: 'Would you like to reorder the checklist items?',
+      },
+    ])
+    .then((answers) => {
+      if (answers.reorder) {
+        return reorder(callback);
+      }
+
+      return callback();
+    });
+}
+
 function init() {
   return inquirer
     .prompt([
       {
         type: 'list',
         name: 'action',
-        choices: ['Run', 'Add', 'Remove', 'Edit'],
+        choices: ['Run', 'Add', 'Remove', 'Edit', 'Reorder'],
         message: 'What do you want to do?',
       },
     ])
     .then(({ action }) => {
       switch (action) {
         case 'Add': {
-          return init();
+          return add(init);
         }
 
         default:
