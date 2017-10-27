bd29ee9fdaf567cb7f4d037b118c18cd1d853006
Charlie Jackson <charlie@charliejackson.com>
2017-10-27 14:35:11 +0100

    Added documentation to Add Input
    Trello card: https://trello.com/c/dhTH7r9i

--- src/components/AddInput/AddInput.js
+++ src/components/AddInput/AddInput.js
@@ -3,16 +3,38 @@ import { View, TextInput } from 'react-native';
 import PropTypes from 'prop-types';
 import style from 'components/AddInput/AddInput.style';
 
+/**
+ * The text input for writing a new mantra
+ *
+ * @type {Class}
+ */
 class AddInput extends Component {
+  /**
+   * Initiate the class, bind the methods.
+   *
+   * @param  {Object} props The passed component props
+   * @return {Void}       No return value
+   */
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
   }
 
+  /**
+   * When the input changes, pass the text to the handler
+   *
+   * @param  {Event} event The event object from the input
+   * @return {Void}       No return value
+   */
   onChange(event) {
     this.props.onChange(event.nativeEvent.text);
   }
 
+  /**
+   * Render the component
+   *
+   * @return {Component} React component to render
+   */
   render() {
     return (
       <View style={style.container}>
