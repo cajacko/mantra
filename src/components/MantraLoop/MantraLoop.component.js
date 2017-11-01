601957d5dd5cec60c5599d6ad7dbad95979b74ae
Charlie Jackson <charlie@charliejackson.com>
2017-11-01 12:06:26 +0000

    Split returnMantraLoop into seperate file
    Trello card: https://trello.com/c/JZwrTYQt

--- src/components/MantraLoop/MantraLoop.component.js
+++ src/components/MantraLoop/MantraLoop.component.js
@@ -1,53 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MantraLoopRender from 'components/MantraLoop/MantraLoop.render';
-
-/**
- * Transform the items from the store into and array of ordered mantra ID's,
- * optionally filtered by title
- *
- * @param  {Object} items The items object from the store. Keyed object of
- * mantra
- * @param  {?String} filterValue The string to filter the mantrs titles by
- * @return {array}       Array of strings, representing Mantra ID's
- */
-function returnMantraLoop(items, filterValue) {
-  const mantraLoop = [];
-  const ids = Object.keys(items);
-
-  let noItems = true;
-
-  ids.forEach((id) => {
-    const item = items[id];
-    let pushItem = false;
-
-    if (filterValue) {
-      const title = item.title.toLowerCase();
-      const test = filterValue.toLowerCase();
-
-      if (title.includes(test)) {
-        pushItem = item;
-      }
-    } else {
-      pushItem = item;
-    }
-
-    if (pushItem) {
-      mantraLoop.push(pushItem);
-
-      if (pushItem.deleted === false) {
-        noItems = false;
-      }
-    }
-  });
-
-  mantraLoop.sort((a, b) => b.dateAdded - a.dateAdded);
-
-  const idLoop = [];
-  mantraLoop.forEach(({ id }) => idLoop.push({ key: id }));
-
-  return { idLoop, noItems };
-}
+import returnMantraLoop from 'helpers/returnMantraLoop';
 
 /**
  * MantraLoop component, handle refreshes, sync errors and whether to update
