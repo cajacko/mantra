cbaaf4e2f3c4d9eefca409e6004f27e66820ea90
Charlie Jackson <contact@charliejackson.com>
2018-03-23 11:17:56 +0000

    Adjust the toast bottom margin based on whether the keyboard is up or not
    Trello card: https://trello.com/c/OpRmX84I

--- src/helpers/toast.js
+++ src/helpers/toast.js
@@ -1,13 +1,18 @@
 import { Toast } from 'native-base';
+import { getIsKeyboardUp, getKeyboardHeight } from 'helpers/keyboard';
 
 function toast(text, type, duration = 4000, buttonText = 'Dismiss') {
-  Toast.show({
+  const options = {
     text,
     type,
     position: 'bottom',
     buttonText,
     duration,
-  });
+  };
+
+  if (getIsKeyboardUp()) options.style = { marginBottom: getKeyboardHeight() };
+
+  Toast.show(options);
 }
 
 export default toast;
